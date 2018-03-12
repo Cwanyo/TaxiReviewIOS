07ec1f221e6adc00041605dd449b39f6bf2e0609
ios-project <ios-project@vilis-macbook.local>
2018-03-12 19:29:49 +0700

    Initial Commit

---
+++ TaxiReviewIOS/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  TaxiReviewIOS
+//
+//  Created by ios-project on 12/3/2561 BE.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
