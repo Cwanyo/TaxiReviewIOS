de82290a6aacd54af33625a4049f780ba944151e
ios-project <ios-project@Vilis-MacBook.local>
2018-03-21 02:05:27 +0700

    add history func

--- TaxiReviewIOS/Controllers/HistoryViewController.swift
+++ TaxiReviewIOS/Controllers/HistoryViewController.swift
@@ -11,14 +11,51 @@ class HistoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateView(index: 0)
+    }
+    
+    // recent view
+    lazy var recentViewController: RecentViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "recentViewController") as! RecentViewController
+        
+        self.addViewControllerAsChildViewController(childViewController: viewController)
+        
+        return viewController
+    }()
+    
+    // myreview view
+    lazy var myReviewViewController: MyReviewViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "myReviewViewController") as! MyReviewViewController
+        
+        self.addViewControllerAsChildViewController(childViewController: viewController)
+        
+        return viewController
+    }()
+    
+    func addViewControllerAsChildViewController(childViewController: UIViewController){
+        addChildViewController(childViewController)
+        
+        view.addSubview(childViewController.view)
+        
+        childViewController.view.frame = view.bounds
+        childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                
+        childViewController.didMove(toParentViewController: self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func selectionDidChange(_ sender: UISegmentedControl) {
+        updateView(index: sender.selectedSegmentIndex)
     }
-
-
+    
+    func updateView(index: Int){
+        recentViewController.view.isHidden = !(index == 0)
+        myReviewViewController.view.isHidden = (index == 0)
+    }
+    
 }
 
